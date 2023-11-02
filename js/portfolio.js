48fc0002ad3f0a630cf8e7442524a33f8cbda24f
eu-nae <143784684+eu-nae@users.noreply.github.com>
2023-11-02 17:49:51 +0900

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,24 @@
+$(function(){
+
+    // $("header>nav>ul>li:nth-child(1)").on("click", function(){
+    //     var about = $("#about").position();
+    //     $("body").animate({
+    //         scrolltop:about.top - 900
+    //     },0);
+
+    //     $("header>nav>ul>li:nth-child(1)").removeClass("active");
+    //     $(this).addClass("active");
+    // });
+
+
+    // $('#wrap').fullpage({
+    //     lookAnchors:false,
+    //     navigation:true,
+    //     navigationPosition:'right',
+    //     navigationTooltips:['Main','About','Skills&Tools','Pf1','Pf2','Pf3','Pf4','Pf5','Other Design'],
+    //     sectionsColor:['#ffffff'],
+    //     showActiveTooltip: true,
+
+    // });
+
+});
